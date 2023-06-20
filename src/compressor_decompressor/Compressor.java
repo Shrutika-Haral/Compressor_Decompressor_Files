f9040823653abb19bfc28ec4552b4a8838f8b24d
Shrutika <shrutikaharal08@gmail.com>
2023-06-20 17:55:39 +0530

    Initial commit

---
+++ src/compressor_decompressor/Compressor.java
@@ -0,0 +1,39 @@
+package compressor_decompressor;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Shrutika
+ */
+import  java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+public class Compressor {
+    public static void method(File file) throws IOException {
+        String fileDirectory=file.getParent();
+        FileInputStream fis = new FileInputStream(file);
+        FileOutputStream fos = new FileOutputStream(fileDirectory+"/Compressesd_File.gz");
+        GZIPOutputStream gzip = new GZIPOutputStream(fos);
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while((len=fis.read(buffer))!=-1){
+            gzip.write(buffer,0,len);
+        }
+        gzip.close();
+        fos.close();
+        fis.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        File path=new File("");
+        method(path);
+    }
+}
+
---
+++ src/compressor_decompressor/Decompressor.java
@@ -0,0 +1,38 @@
+package compressor_decompressor;/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Shrutika
+ */
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+
+public class Decompressor {
+    public static void method(File file) throws IOException {
+        String fileDirectory = file.getParent();
+
+        FileInputStream fis = new FileInputStream(file);
+        GZIPInputStream gzip = new GZIPInputStream(fis);
+        FileOutputStream fos = new FileOutputStream(fileDirectory + "/Decompressed_File");
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = gzip.read(buffer)) != -1) {
+            fos.write(buffer, 0, len);
+        }
+        gzip.close();
+        fos.close();
+        fis.close();
+    }
+    public static void main(String[] args) throws IOException {
+        File path = new File("");
+        method(path);
+    }
+}
